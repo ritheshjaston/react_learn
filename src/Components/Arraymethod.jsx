29205756af291f33952dbecbfce09c0ee77a95b0
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-05 12:55:39 +0530

    arraymethod

--- src/Components/Arraymethod.jsx
+++ src/Components/Arraymethod.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Arraymethod() {
     const fruits=['Apple','Banana','mango'];
+
+    const student=[{
+        name:"Rithesh",
+        phone:7795113043,
+        address:"Manglore"
+    }];
+    console.log(student.address);
     // fruits.map(()=>{});
   return (
     <div>
@@ -12,6 +19,16 @@ export default function Arraymethod() {
       })}
       {fruits.map((value)=>
         <h3>{value}</h3>)}
+
+      {student.map((data)=>
+      <>
+        <h3>{data.name}</h3>
+        <h3>{data.phone}</h3>
+        <h3>{data.address}</h3>
+      </>
+        )}
+
+        
     </div>
   )
 }
