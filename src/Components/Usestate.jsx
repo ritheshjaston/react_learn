bb506e26ef0aea4205db7ee07289cdf0d833acab
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-02-09 11:49:32 +0530

    usestate object add from form data

--- src/Components/Usestate.jsx
+++ src/Components/Usestate.jsx
@@ -1,35 +1,45 @@
 import React from 'react'
 import { useState } from 'react'
 export default function Usestate() {
-    const [color, setcolor]=useState("red");
-    const [count, setcount]=useState(0);
-    const [obj, setobj]=useState([{
-        name:"Rithesh"
+    const [color, setcolor] = useState("red");
+    const [count, setcount] = useState(0);
+    const [data, setdata] = useState({});
+    const [obj, setobj] = useState([{
+        name: "Rithesh"
     }]);
     console.log(color);
     console.log(obj);
 
-    const handleclick=()=>{
-        if(color=="yellow"){
+    const handleclick = () => {
+        if (color == "yellow") {
 
             setcolor("red");
-        }else{
+        } else {
             setcolor("yellow");
-            
+
         }
     }
-   
-  return (
-    <div>
-      <h1>Use State Hook</h1>
-        {color} <br />
-        <button onClick={handleclick}>click</button>
+    const handler=(e)=>{
+        setdata({ [e.target.name]: e.target.value })
+        console.log(data)
+    }
 
-        <div style={{background:color,width:"200px",height:"200px"}}>
+    return (
+        <div>
+            <h1>Use State Hook</h1>
+            {color} <br />
+            <button onClick={handleclick}>click</button>
 
+            <div style={{ background: color, width: "200px", height: "200px" }}>
+
+            </div>
+            <h1>{count}</h1>
+            <button onClick={() => setcount(count + 1)}>Click Here</button>
+
+
+            {/* form data */}
+            <input type="text" name='myname' onChange={handler} />
+            <h1>{data.myname}</h1>
         </div>
-        <h1>{count}</h1>
-        <button onClick={()=>setcount(count+1)}>Click Here</button>
-    </div>
-  )
+    )
 }
